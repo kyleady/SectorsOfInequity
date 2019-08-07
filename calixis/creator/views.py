0044fef8c4358b2c486fea0ad5be63995bf59a6f
Kyle Smith <kyledavidady@gmail.com>
2019-08-07 07:16:33 -0700

    Clean up calixis debug.

--- calixis/creator/views.py
+++ calixis/creator/views.py
@@ -4,6 +4,4 @@ import requests
 def index(request):
     r = requests.post('http://127.0.0.1:8080/grid/', json={})
     output = r.json()
-    print(r)
-    print(output)
     return JsonResponse(output)
