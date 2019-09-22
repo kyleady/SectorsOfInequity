2b4183b0703ecf009e1d616f2925f8c7761d7545
Kyle Smith <kyledavidady@gmail.com>
2019-09-22 00:41:49 -0700

    Create generic views, templates, and urls for create, list, and edit config models.

--- calixis/calixis/settings.py
+++ calixis/calixis/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '#b4#f-63p08q-^jqz(zq*n=nx^aukz_#a3fkk09r8(tr+v(9#b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
